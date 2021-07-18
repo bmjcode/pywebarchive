6751f27282987c9e5df0c65dcdde52ddcee72b49
Benjamin Johnson <42893476+bmjcode@users.noreply.github.com>
2021-07-18 09:25:45 -0400

    Add an option to enable single-file extraction in extractor.py

--- extractor.py
+++ extractor.py
@@ -4,6 +4,7 @@
 
 import os
 import sys
+import optparse
 
 import webarchive
 
@@ -11,28 +12,36 @@ import webarchive
 def main():
     """Extract the .webarchive file specified on the command line."""
 
-    if len(sys.argv) < 2:
-        # Display an error message and exit
-        print("Usage: {0} filename.webarchive [output.html]"
-              .format(sys.argv[0]),
-              file=sys.stderr)
-        sys.exit(1)
+    parser = optparse.OptionParser(
+        usage="%prog [options] input_path.webarchive [output_path.html]"
+    )
+    parser.add_option("-s", "--single-file",
+                      action="store_true", dest="single_file", default=False,
+                      help="extract archive contents to a single HTML file "
+                           "with embedded resources (experimental)")
 
-    # Get the archive path from the command line
-    archive_path = sys.argv[1]
+    options, args = parser.parse_args()
+    if len(args) == 1:
+        # Get the archive path from the command line
+        archive_path = args[0]
 
-    if len(sys.argv) >= 3:
-        # Get the output path from the command line
-        output_path = sys.argv[2]
-
-    else:
         # Derive the output path from the archive path
         base, ext = os.path.splitext(archive_path)
         output_path = "{0}.html".format(base)
 
+    elif len(args) == 2:
+        # Get the archive and output paths from the command line
+        archive_path, output_path = args
+
+    else:
+        # Print the correct usage and exit
+        parser.print_usage(file=sys.stderr)
+        sys.exit(1)
+
     # Extract the archive
     archive = webarchive.open(archive_path)
-    archive.extract(output_path)
+    archive.extract(output_path,
+                    single_file=options.single_file)
 
 
 if __name__ == "__main__":
