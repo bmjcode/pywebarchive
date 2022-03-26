c22a29aa1abdc3fca71a2262f4999b3f2dd9b55c
Benjamin Johnson <42893476+bmjcode@users.noreply.github.com>
2022-03-26 15:40:00 -0400

    Release this as version 0.4.0

--- webarchive/__init__.py
+++ webarchive/__init__.py
@@ -26,7 +26,7 @@ from .webarchive import WebArchive
 from .webresource import WebResource
 from .exceptions import WebArchiveError
 
-__version__ = "0.3.3"
+__version__ = "0.4.0"
 
 __all__ = [
     "__version__",
